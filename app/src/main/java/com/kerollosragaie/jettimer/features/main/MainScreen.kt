b16262d0fd3a2e4fb10ccd0c439fc998a38d2689
kerolosragaie <kerolosragaie@gmail.com>
2023-12-14 11:46:31 +0200

    adding TopAppBar

--- app/src/main/java/com/kerollosragaie/jettimer/features/main/MainScreen.kt
+++ app/src/main/java/com/kerollosragaie/jettimer/features/main/MainScreen.kt
@@ -1,11 +1,23 @@
 package com.kerollosragaie.jettimer.features.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.kerollosragaie.jettimer.features.main.components.TimerItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     mainViewModel: MainViewModel,
@@ -13,12 +25,37 @@ fun MainScreen(
     val currentTime by mainViewModel.currentTime.collectAsState()
     val isTimerRunning by mainViewModel.isTimerRunning.collectAsState()
 
-    TimerItem(
-        currentTimer = currentTime,
-        isRunning = isTimerRunning,
-        onStart = { mainViewModel.startTimer() },
-        onReStart = { mainViewModel.restartTimer() },
-    )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onSecondary
+                ),
+                title = {
+                    Text(
+                        text = "Timer",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                    )
+                },
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            TimerItem(
+                currentTimer = currentTime,
+                isRunning = isTimerRunning,
+                onStart = { mainViewModel.startTimer() },
+                onReStart = { mainViewModel.restartTimer() },
+            )
+        }
+    }
 }
 
 
